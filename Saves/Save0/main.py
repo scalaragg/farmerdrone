4ea4a33da5d13a565074feaee8b3465fb5ae7a5d
scalaragg <dlaeve321@gmail.com>
2026-09-23 09:03:46 +0300

    perviy soxran

---
+++ Saves/Save0/main.py
@@ -0,0 +1,69 @@
+while True:
+	if get_pos_x() == 0 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+		move(East)
+	elif get_pos_x() == 1 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+		move(East)
+	elif get_pos_x() == 2 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		plant(Entities.Carrot)
+		move(North)
+		move(East)
+	elif get_pos_x() == 3 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+		move(East)
