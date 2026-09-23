3bb7c91320defe9b8135349278a7bbcfb410581d
scalaragg <dlaeve321@gmail.com>
2026-09-23 09:23:23 +0300

    vtoroy seiv

--- Saves/Save0/main.py
+++ Saves/Save0/main.py
@@ -15,6 +15,14 @@ while True:
 		harvest()
 		#grass
 		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		#grass
+		move(North)
+	elif get_pos_x() == 0 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		#grass
+		move(North)
 		move(East)
 	elif get_pos_x() == 1 and get_pos_y() == 0 and can_harvest():
 		harvest()
@@ -32,21 +40,43 @@ while True:
 		harvest()
 		plant(Entities.Bush)
 		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 1 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
 		move(East)
 	elif get_pos_x() == 2 and get_pos_y() == 0 and can_harvest():
 		harvest()
+		till()
 		plant(Entities.Carrot)
 		move(North)
 	elif get_pos_x() == 2 and get_pos_y() == 1 and can_harvest():
 		harvest()
+		till()
 		plant(Entities.Carrot)
 		move(North)
 	elif get_pos_x() == 2 and get_pos_y() == 2 and can_harvest():
 		harvest()
+		till()
 		plant(Entities.Carrot)
 		move(North)
 	elif get_pos_x() == 2 and get_pos_y() == 3 and can_harvest():
 		harvest()
+		till()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Carrot)
+		move(North)
+	elif get_pos_x() == 2 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		till()
 		plant(Entities.Carrot)
 		move(North)
 		move(East)
@@ -65,5 +95,75 @@ while True:
 	elif get_pos_x() == 3 and get_pos_y() == 3 and can_harvest():
 		harvest()
 		plant(Entities.Bush)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		plant(Entities.Tree)
+		move(North)
+	elif get_pos_x() == 3 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		plant(Entities.Bush)
+		move(North)
+		move(East)
+	elif get_pos_x() == 4 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 4 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+		move(East)
+	elif get_pos_x() == 5 and get_pos_y() == 0 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 1 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 2 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 3 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 4 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
+		move(North)
+	elif get_pos_x() == 5 and get_pos_y() == 5 and can_harvest():
+		harvest()
+		till()
+		plant(Entities.Pumpkin)
 		move(North)
 		move(East)
